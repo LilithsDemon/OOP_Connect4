11ec47d6de046d659a2d3d280997c5084a5d0fcb
ws287176 <ws287176@weston.ac.uk>
2023-11-10 19:56:29 +0000

    Working and have a demo

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,39 +1,75 @@
 ﻿using System;
 using System.Dynamic;
-using gui_connect4;
+using BoardLogic;
 
-namespace main_connect4
+namespace MainConnect4
 {
     class Program
     {
 
         public class User
         {
-            public string username {get; private set;}
-            public string colour {get; private set;}
-            public int wins {get; private set;} = 0;
+            public string Username {get; private set;}
+            public ConsoleColor Colour {get; private set;}
+            public int Wins {get; private set;} = 0;
 
             public void AddWin()
             {
-                wins++;
+                this.Wins++;
             }
 
-            public User(string name, string given_colour)
+            public User(string name, ConsoleColor given_colour)
             {
-                username = name;
-                colour = given_colour;
+                this.Username = name;
+                this.Colour = given_colour;
             }
         }
 
         static void Main()
         {
-            int[ , ] rows = Board.FillBoard(6, 7, 0);
-            User[] users = new User[] {new User("Test1", "red"), new User("Test2", "blue")};
-            rows[5,0] = 1;
-            rows[5,1] = 2;
-            rows[4,0] = 1;
-            rows[3,0] = 2;
-            Board.DisplayBoard(rows, "red", "blue");
+            User[] users = new User[] {new User("Player 1", ConsoleColor.Red), new User("Player 2", ConsoleColor.Blue)};
+            Board board = new Board(users[0].Colour, users[1].Colour);
+            bool player = false; // If false, player 1, if True player 2
+            int pos = 0;
+            bool can_place = true;
+            while(true)
+            {
+                board.DisplayBoard();
+                bool found = false;
+                while(!found)
+                {
+                    string output_text = player == false ? $"{users[0].Username} Where would you Like to place your counter:" : $"{users[1].Username} Where would you Like to place your counter:";
+                    Console.WriteLine(output_text);
+                    string user_input = Console.ReadLine() ?? "";
+                    if(user_input == "")
+                    { 
+                        Console.WriteLine("Please enter a value");
+                        continue;
+                    }
+                    found = int.TryParse(user_input, out pos);
+                    if(found == false)
+                    {
+                        Console.WriteLine("Please enter a numerical value");
+                        continue;
+                    }
+                    if(pos > 7 || pos < 1)
+                    {
+                        Console.WriteLine("Please enter a value bigger than 0 and smaller than 8");
+                        found = false;
+                        continue;
+                    }
+
+                    if(player == false) can_place = board.PlaceCounter(pos, 1);
+                    else can_place = board.PlaceCounter(pos, 2);
+
+                    if(!can_place)
+                    {
+                        Console.WriteLine("You cannot place that there as that row is full!");
+                        found = false;
+                    }
+                }
+                player = !player;
+            }
         }
     }
 }
---
+++ Connect4/board.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Drawing;
+
+namespace BoardLogic
+{
+    public sealed class Board
+    {
+        private ConsoleColor[] Colours = new ConsoleColor[3];
+        public int[,] WholeBoard {get; private set;} = FillBoard(0);
+
+        public static int[,] FillBoard (int fill_val)
+        {
+            int[,] tempArray = new int[6,7];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    tempArray[i, j] = fill_val;
+                }
+            }
+            return tempArray;
+        }
+
+        public void DisplayBoard()
+        {
+            Console.Clear();
+            int current_val = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < 7; j++)
+                {
+                    current_val = this.WholeBoard[i, j];
+                    Console.ForegroundColor = this.Colours[current_val];
+                    string output = current_val != 0 ? "O" : " ";
+                    Console.Write(output);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" | ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine(String.Concat(Enumerable.Repeat("-", 29)));
+            Console.Write("|");
+            for(int i = 0; i < 7; i++)
+            {
+                Console.Write($" {i+1} |");
+            }
+            Console.WriteLine("");
+        }
+
+        public bool PlaceCounter(int pos, int player)
+        {
+            pos--;
+            for(int i = 5; i >= 0; i--)
+            {
+                if(this.WholeBoard[i, pos] == 0)
+                {
+                    this.WholeBoard[i, pos] = player;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Board(ConsoleColor player_1_colour, ConsoleColor player_2_colour, ConsoleColor board_colour = ConsoleColor.White)
+        {
+            Colours[0] = board_colour;
+            Colours[1] = player_1_colour;
+            Colours[2] = player_2_colour;
+        }
+    }
+}
